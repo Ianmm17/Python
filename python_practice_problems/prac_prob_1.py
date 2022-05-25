61298a2908d2c551c73f253a37e4ce2cfc68d76e
ian <ianmm17@yahoo.com>
2022-05-24 22:22:37 -0500

    practice problems using Python

---
+++ python_practice_problems/prac_prob_1.py
@@ -0,0 +1,6 @@
+# Double the integer and return it.
+
+def double_integer(i):
+    return i * 2
+
+print(double_integer(3))
---
+++ python_practice_problems/prac_prob_2.py
@@ -0,0 +1,18 @@
+''' 
+sum the differences between consecutive 
+pairs in the array in descending order.
+'''
+
+def sum_of_differences(arr):
+    arr.sort(reverse= True)
+    if len(arr )<= 1:
+        return 0
+    else:
+        for i in arr:
+            
+            
+    return 0
+
+
+arr = [1,3,4,2]
+sum_of_differences(arr)
